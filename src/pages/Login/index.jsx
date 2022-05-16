c0e32923b0b78f94c839f268be1e9166c81cbefb
Aguimar Junior <96925562+Aguimar-Junior@users.noreply.github.com>
2022-05-16 18:39:30 -0300

    Criação da tela de Login

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import logo from '../../img/devBank-logo.png'
+
+import "./login.css";
+
+const Login = () => {
+    return (
+        <div className="container">
+            <form className="form">
+                <img src={logo} className="logo"/>
+                <h1 className="title">Login</h1>
+                <input className="handleInput" type='email' placeholder='E-mail' required/>
+                <input className="handleInput" type='password' placeholder='Senha' required/>
+                <button className="button" type="submit">Acessar</button>
+                <p className="text">Não possui uma conta?</p>
+                <Link className="link" to="/register">Cadastre-se</Link>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+import "./register.css"
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+            <Link to="/">Login</Link>
+        </div>
+    )
+}
+
+export default Register
