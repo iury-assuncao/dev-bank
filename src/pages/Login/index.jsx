cc7384a7c533c1f39480e558ba55248b39719375
Gustavo Gabriel Pereira Biolcatti <43222626+GustavoBiolcatti@users.noreply.github.com>
2022-05-16 22:29:19 -0300

    tela de cadastro e alteração de nomenclatura
    * Criação da tela de cadastro
    * Inclusão do useState das telas de login e cadastro
    * Alteração na nomenclatura do CSS para o BEM

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
 import logo from '../../img/devBank-logo.png'
 
 import "./login.css";
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <div className="container">
-            <form className="form">
-                <img src={logo} className="logo" alt="Logo DevBank"/>
-                <h1 className="title">Login</h1>
-                <input className="handleInput" type='email' placeholder='E-mail' required/>
-                <input className="handleInput" type='password' placeholder='Senha' required/>
-                <button className="button" type="submit">Acessar</button>
-                <p className="text">Não possui uma conta?</p>
-                <Link className="link" to="/register">Cadastre-se</Link>
+            <form className="login">
+                <img src={logo} className="login__logo" alt="Logo DevBank"/>
+                <h1 className="login__title">Login</h1>
+
+                <input className="login__handleInput" type='email' value={email} onChange={e => setEmail(e.target.value.trim())} placeholder='E-mail' required/>
+                <input className="login__handleInput" type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Senha' required/>
+                <button className="login__button" type="submit">Acessar</button>
+                
+                <p className="login__text">Não possui uma conta?</p>
+                <Link className="login__link" to="/register">Cadastre-se</Link>
             </form>
         </div>
     )
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,12 +1,38 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import logo from '../../img/devBank-logo.png'
 
 import "./register.css"
 
 function Register() {
+
+    const [name, setName] = useState()
+    const [cpf, setCpf] = useState()
+    const [email, setEmail] = useState()
+    const [birth, setBirth] = useState()
+    const [password, setPassword] = useState()
+    const [confirmPassword, setConfirmPassword] = useState()
+
     return (
-        <div>
-            <h1>Register</h1>
-            <Link to="/">Login</Link>
+        <div className="container">
+            <form className="register">
+                <img src={logo} className="register__logo" alt="Logo DevBank"/>
+                <h1 className="register__title">Cadastro</h1>
+
+                <fieldset className="register__fieldset">
+                    <input className="register__handleInput register__handleInput--large" type='text' value={name} onChange={e => setName(e.target.value.trim())} placeholder='Nome' required/>
+                    <input className="register__handleInput" type='text' value={cpf} onChange={e => setCpf(e.target.value.trim())} placeholder='CPF' required/>
+                    <input className="register__handleInput register__handleInput--large" type='email' value={email} onChange={e => setEmail(e.target.value.trim())} placeholder='E-mail' required/>
+                    <input className="register__handleInput" type='date' value={birth} onChange={e => setBirth(e.target.value.trim())} placeholder='Nascimento' required/>
+                    <input className="register__handleInput register__handleInput--medium" type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Senha' required/>
+                    <input className="register__handleInput register__handleInput--medium" type='password' value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder='Confirme sua senha' required/>
+                </fieldset>
+                <button className="register__button" type="submit">Acessar</button>
+                
+                <p className="register__text">Já possui uma conta?</p>
+                <Link to="/" className="register__link">Entre</Link>
+            </form>
         </div>
     )
 }
