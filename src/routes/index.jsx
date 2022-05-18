cdb66172f21ad5b3f4100a20d845e885cb54eaa9
Gustavo Gabriel Pereira Biolcatti <43222626+GustavoBiolcatti@users.noreply.github.com>
2022-05-17 21:03:09 -0300

    Criação da tela de loading e suspense/lazy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,34 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Dashboard from "../pages/Dashboard";
+const Login = lazy(() => import("../pages/Login"))
+const Register = lazy(() => import("../pages/Register"))
+const Dashboard = lazy(() => import("../pages/Dashboard"))
+
+import Loading from "../components/Loading"
 
 function Rotas() {
     return (
         <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard/*" element={<Dashboard/>} />
+            <Route path="/" element={
+                <Suspense fallback={<Loading />}>
+                    <Login />
+                </Suspense>
+            } />
+
+            <Route path="/register" element={
+                <Suspense fallback={<Loading />}>
+                    <Register />
+                </Suspense>
+            } />
+
+            <Route path="/dashboard/*" element={
+                <Suspense fallback={<Loading />}>
+                    <Dashboard />
+                </Suspense>
+            } />
         </Routes>
     )
 }
 
-export default Rotas;
+export default Rotas
