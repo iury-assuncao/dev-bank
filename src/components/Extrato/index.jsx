c19e07ec3d3873ce84331371df3fdb33637a868c
Gustavo Biolcatti <43222626+gustavobiolcatti@users.noreply.github.com>
2022-05-19 17:05:20 -0300

    criação de tela de extrato
    * Criação da tela de extrato e a função que pega os dados da API

--- src/components/Extrato/index.jsx
+++ src/components/Extrato/index.jsx
@@ -1,8 +1,56 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+import Loading from "../Loading"
+
+import "./extrato.css"
 
 const Extrato = () => {
+
+    const [transations, setTransations] = useState([])
+
+    const { loading, getExtract } = useContext(AuthContext)
+
+    useEffect(() => {
+        const loadExtract = async () => {
+            setTransations(await getExtract())
+        }
+
+        loadExtract()
+    }, [])
+
     return(
-        <div>
-            Extrato
+        <div className="statement">
+            {
+                loading
+                ? <Loading />
+                :
+                transations.length === 0
+                    ? <h2>Sem transações</h2>
+                    : 
+                    <table className="statement__list">
+                        <thead className="statement__list-header">
+                            <tr>
+                                <th>Valor</th>
+                                <th>Tipo</th>
+                                <th>Data</th>
+                            </tr>
+                        </thead>
+                        <tbody className="statement__list-content">
+                            {
+                                transations.map(item => {
+                                    return (
+                                        <tr key={item._id}>
+                                            <td className={item.tipo === "ENTRADA" ? "statement__value--income" : "statement__value--expense"}>{parseFloat(item.valor).toFixed(2)}</td>
+                                            <td>{item.tipo}</td>
+                                            <td>{(new Date(item.data)).toLocaleDateString()}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+            }
         </div>
     )
 }
--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,10 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+
+    const [loading, setLoading] = useState(false)
+
+    const apiUrl = "https://api-contas-trade4devs.herokuapp.com"
+
+    const getExtract = async () => {
+        setLoading(true)
+
+        //Alterar o ID do usuário na URL
+        const { data: { operacoes } } = await axios.get(`${apiUrl}/conta/extrato/12345678912/{mes}`).finally(() => setLoading(false))
+
+        return operacoes
+    }
+
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{loading, getExtract}}>
             { children }
         </AuthContext.Provider>
     )
