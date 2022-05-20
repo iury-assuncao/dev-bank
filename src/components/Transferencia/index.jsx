2507a53dddc4beaa3b706db35494b45e9aab72d1
Aguimar Junior <96925562+Aguimar-Junior@users.noreply.github.com>
2022-05-20 15:34:35 -0300

    Ajustes/componente de transferencia

--- src/components/Transferencia/index.jsx
+++ src/components/Transferencia/index.jsx
@@ -2,9 +2,15 @@ import './transferencia.css';
 
 const Transferencia = () => {
     return(
-        <div>
-            transferencia
-        </div>
+        <div className='container__transfer'>
+                        <form className='transfer'>
+                            <p className='transfer__text'>Qual é o valor da transferência?</p>
+                            <input className='transfer__input__value' type="number" placeholder='0,00' />
+                            <p className='transfer__text__recipient'>Para quem deseja fazer a transferência?</p>
+                            <input className='transfer__input__recipient' type="text" placeholder='CPF do destinatário' />
+                            <button className='transfer__button'>Confirmar</button>
+                        </form>
+                    </div>
     )
 }
 
