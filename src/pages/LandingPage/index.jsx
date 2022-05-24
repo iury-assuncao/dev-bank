8e9914e64d059d131c9dc73b595f0ee83ce31b7f
Iury Assunção <iuryifba@gmail.com>
2022-05-24 09:12:27 -0300

    header landing page

---
+++ src/pages/LandingPage/index.jsx
@@ -0,0 +1,15 @@
+import './landingPage.css';
+import HeaderLanding from '../../components/HeaderLanding';
+
+const LandingPage = () =>{
+    return(
+        <main>
+
+        <HeaderLanding />
+
+
+        </main>
+    );
+}
+
+export default LandingPage;
