32f075d00032aea242be38ac8d56b93fc8a49d25
Iury Assunção <iuryifba@gmail.com>
2022-05-24 14:22:08 -0300

    section cards finalizada

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,12 +1,15 @@
 import './landingPage.css';
 import HeaderLanding from '../../components/HeaderLanding';
 import SectionApresentacao from '../../components/SectionApresentacaio';
+import SectionCards from '../../components/SectionCards';
 
 const LandingPage = () =>{
     return(
         <div>
             <HeaderLanding />
             <SectionApresentacao/>
+            <SectionCards />
+
         </div>
     );
 }
