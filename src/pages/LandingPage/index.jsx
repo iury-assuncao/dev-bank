c2b1b63f963918b8a632653201c71cf2891e07db
Iury Assunção <iuryifba@gmail.com>
2022-05-25 16:31:24 -0300

    footer criado

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -4,15 +4,18 @@ import SectionApresentacao from '../../components/SectionApresentacao';
 import SectionCards from '../../components/SectionCards';
 import SectionOperacoes from '../../components/SectionOperacoes';
 import SectionSobre from '../../components/SectionSobre';
-
+import FooterLanding from '../../components/FooterLanding';
 const LandingPage = () =>{
     return(
         <div>
             <HeaderLanding />
-            <SectionApresentacao/>
-            <SectionCards />
-            <SectionOperacoes/>
-            <SectionSobre/>
+            <main>
+                <SectionApresentacao/>
+                <SectionCards />
+                <SectionOperacoes/>
+                <SectionSobre/>
+            </main>
+            <FooterLanding />
         </div>
     );
 }
