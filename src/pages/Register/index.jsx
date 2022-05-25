e9802853891d70f4c3c13391218d661eba11bc36
Gustavo Biolcatti <43222626+gustavobiolcatti@users.noreply.github.com>
2022-05-25 12:36:55 -0300

    Correção de bugs - cadastro

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -29,10 +29,10 @@ function Register() {
         }
 
         const data = { 
-            nome: name, 
-            telefone: tel,
-            email, 
-            cpf, 
+            nome: name.trim(), 
+            telefone: tel.trim(),
+            email: email.trim(), 
+            cpf: cpf.trim(), 
             dataNascimento: birth, 
             senha: password 
         };
@@ -51,11 +51,11 @@ function Register() {
                         <h1 className="register__title">Cadastro</h1>
 
                         <fieldset className="register__fieldset">
-                            <input className="register__handleInput register__handleInput--medium" type='text' value={name} onChange={e => setName(e.target.value.trim())} placeholder='Nome' required/>
-                            <input className="register__handleInput register__handleInput--medium" type='email' value={email} onChange={e => setEmail(e.target.value.trim())} placeholder='E-mail' required/>
-                            <input className="register__handleInput" type='tel' value={tel} onChange={e => setTel(e.target.value.trim())} placeholder='Telefone' required/>
-                            <input className="register__handleInput" type='text' value={cpf} onChange={e => setCpf(e.target.value.trim())} placeholder='CPF' required/>
-                            <input className="register__handleInput" type='date' value={birth} onChange={e => setBirth(e.target.value.trim())} placeholder='Nascimento' required/>
+                            <input className="register__handleInput register__handleInput--medium" type='text' value={name} onChange={e => setName(e.target.value)} placeholder='Nome' required/>
+                            <input className="register__handleInput register__handleInput--medium" type='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='E-mail' required/>
+                            <input className="register__handleInput" type='tel' value={tel} onChange={e => setTel(e.target.value)} placeholder='Telefone' required/>
+                            <input className="register__handleInput" type='text' value={cpf} onChange={e => setCpf(e.target.value)} placeholder='CPF' required/>
+                            <input className="register__handleInput" type='date' value={birth} onChange={e => setBirth(e.target.value)} placeholder='Nascimento' required/>
                             <input className="register__handleInput register__handleInput--medium" type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Senha' required/>
                             <input className="register__handleInput register__handleInput--medium" type='password' value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder='Confirme sua senha' required/>
                         </fieldset>
